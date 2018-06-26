dd050df9b14a553916e65e8ca49729ce32122af6
libraiger <misssilverbeauty0927@outlook.com>
2018-06-26 18:30:28 +0200

    Initialize global variables that represent the game state in the game module

--- game.py
+++ game.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+
+# black (X) = 1, white (O) = 2
+# Note that the AI is passed a modified board with its own marbles represented
+# by 1 and the opponent's marbles by -1.
+currentPlayer = 1
+
+# Maps a space of the board to the player (1/2) to own it or 0 if the space is
+# empty. See the documentation for information on how the spaces are denoted.
+board = {}
+
+# The score of a player decrements whenever a marble is pushed off the board by
+# the opponent.
+score = {'p1': 6, 'p2': 6}
