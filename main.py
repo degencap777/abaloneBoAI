dd20478e2fb2130e138595485b1968a635fceedc
libraiger <misssilverbeauty0927@outlook.com>
2018-06-27 18:25:39 +0200

    Define function `main.initGame`

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+
+import game
+import random
+
+def initGame():
+    """Set the game to its inital state and print the board.
+    """
+
+    game.fillBoard()
+    game.printBoard()
+
+    if random.random() < 0.5:
+        game.togglePlayer()
