510494b6056286956297eaea3a2a47dc1a9fa262
libraiger <misssilverbeauty0927@outlook.com>
2018-06-27 19:17:41 +0200

    Define function `interactivePlayer.turn`

--- interactivePlayer.py
+++ interactivePlayer.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+
+
+def turn(board, opponentMove):
+    """Receive input from the user to make a move.
+
+    :param board: the current state of the board
+    :type board: dict
+    :param opponentMove: the opponent's last move
+    :type opponentMove: tuple(list[str], int) | None
+    :return: the move to be performed
+    :rtype: tuple(list[str], int)
+    """
+
+    while True:
+        try:
+            marbles = input('marbles (space separated): ')
+            marbles = [marble for marble in marbles.split(' ') if marble != '']
+            direction = input('direction [1-9]: ')
+            direction = int(direction)
+            return (marbles, direction)
+        except KeyboardInterrupt as e:
+            raise e
+        except Exception as e:
+            print('Input exception: ' + str(e))
