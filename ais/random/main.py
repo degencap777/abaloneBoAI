e02aac3ff385b5cb486c9bfbf07fc1ed79d98266
libraiger <misssilverbeauty0927@outlook.com>
2018-08-03 18:39:03 +0200

    Update random AI to work with version 2 (#2)
    See Pull Request #1

--- ais/random/main.py
+++ ais/random/main.py
@@ -4,13 +4,15 @@
 # This code is licensed under the MIT License, see LICENSE.md
 
 
-import game
+import abalone
 import random
+import sys
+
+game = sys.modules['__main__'].game
 
 
 def turn(board, opponent_move):
     """Perform a random move.
-
     :param board: the current state of the board
     :type board: dict
     :param opponent_move: the opponent's last move
@@ -19,28 +21,16 @@ def turn(board, opponent_move):
     :rtype: tuple(list[str], int)
     """
 
-    spaces = [
-        'A1', 'A2', 'A3', 'A4', 'A5',
-        'B1', 'B2', 'B3', 'B4', 'B5', 'B6',
-        'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7',
-        'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8',
-        'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'E9',
-        'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9',
-        'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'G9',
-        'H4', 'H5', 'H6', 'H7', 'H8', 'H9',
-        'I5', 'I6', 'I7', 'I8', 'I9'
-    ]
-
     straight_lines = []
-    for space in spaces:
+    for space in abalone.spaces:
         if board[space] == 1:
             straight_lines.append([space])
             for direction in [1, 2, 6]:
-                neighbor1 = game.neighbor(space, direction)
-                if neighbor1 in spaces and board[neighbor1] == 1:
+                neighbor1 = abalone.neighbor(space, direction)
+                if neighbor1 in abalone.spaces and board[neighbor1] == 1:
                     straight_lines.append([space, neighbor1])
-                    neighbor2 = game.neighbor(neighbor1, direction)
-                    if neighbor2 in spaces and board[neighbor2] == 1:
+                    neighbor2 = abalone.neighbor(neighbor1, direction)
+                    if neighbor2 in abalone.spaces and board[neighbor2] == 1:
                         straight_lines.append([space, neighbor1, neighbor2])
 
     moves = []
@@ -48,16 +38,13 @@ def turn(board, opponent_move):
         for direction in range(1, 7):
             off_board = False
             for marble in line:
-                if game.neighbor(marble, direction) == 0:
+                if abalone.neighbor(marble, direction) == 0:
                     off_board = True
                     break
             if off_board:
                 continue
             try:
-                board_copy = {}
-                for space in game.global_board:
-                    board_copy[space] = game.global_board[space]
-                game.move(line, direction, board_copy)
+                game.copy().move(line, direction)
                 moves.append((line, direction))
             except Exception:
                 pass
